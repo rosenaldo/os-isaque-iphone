538610633cb7320fd5a61770d3c56aed105db130
Jose Rosenaldo da S Junior <joserdasjunior@gmail.com>
2024-05-22 19:57:49 -0300

    Ajuste no PDF
    .

--- js/script.js
+++ js/script.js
@@ -18,6 +18,9 @@ document.getElementById('generate-PDF').addEventListener('click', function () {
 function gerarOrdemServico(nome, telefone, email, marca, modelo, imei, defeito, servicos,valorTotal,desconto,imagemPDF,termos) {
     const doc = new jsPDF();
     // Adiciona a imagem no cabeçalho
+    doc.setFillColor(211, 211, 211);     
+    doc.rect(0, 0, 210, 40, 'F');
+
 doc.addImage(imagemPDF, 'PNG', 10, 5, 50, 30);
 
 // Título da Ordem de Serviço
@@ -28,33 +31,33 @@ doc.setFontType("normal");
 
 // CNPJ
 doc.setFontSize(12);
-doc.text(`CNPJ: 111111111111/11`, 145, 25);
+doc.text(`CNPJ: 28.649.899/0001-97`, 145, 25);
 doc.text(`Contato: 84 9 9820-0953`, 145, 20);
 
 // Informações do Cliente
-doc.text(`Nome: ${nome}`, 10, 70);
-doc.text(`Telefone: ${telefone}`, 10, 80);
-doc.text(`Email: ${email}`, 10, 90);
-doc.text(`Marca do Celular: ${marca}`, 10, 100);
-doc.text(`Modelo do Celular: ${modelo}`, 10, 110);
-doc.text(`IMEI do Celular: ${imei}`, 10, 120);
+doc.text(`Cliente: ${nome}`, 10, 50);
+doc.text(`Telefone: ${telefone}`, 90, 50);
+doc.text(`Email: ${email}`, 10, 60);
+doc.text(`Marca do Celular: ${marca}`, 90, 60);
+doc.text(`Modelo do Celular: ${modelo}`, 10, 70);
+doc.text(`IMEI do Celular: ${imei}`, 90, 70);
 
 // Descrição do Defeito e Serviços
-doc.text(`Descrição do Defeito:`, 10, 140);
-doc.text(`${defeito}`, 10, 150);
-doc.text(`Serviços a serem Realizados:`, 10, 170);
-doc.text(`${servicos}`, 10, 180);
+doc.text(`Descrição do Defeito:`, 10, 80);
+doc.text(`${defeito}`, 90, 80);
+doc.text(`Serviços a serem Realizados:`, 10, 100);
+doc.text(`${servicos}`, 90, 100);
 
 // Valor Total e Desconto
-doc.text(`Valor do Serviço: R$ ${valorTotal}`, 10, 210);
-doc.text(`Desconto: R$ ${desconto}`, 10, 200);
+doc.text(`Valor do Serviço: R$ ${valorTotal}`, 90, 120);
+doc.text(`Desconto: R$ ${desconto}`, 10, 120);
 
 // Termos e Condições
 doc.setFontSize(10); 
-doc.text(`A empresa somente se responsabilizará pela entrega deste aparelho,`, 10, 230);
-doc.text(`mediante a apresentação deste comprovante após 90 dias a contar da data de entrega,`, 10, 235);
-doc.text(`a empresa disporá do equipamento afim de custear as despesas conforme, Art. 1275, 1592, 3995,`,10,240);
-doc.text(`do código civil brasileiro, sem prejuízo do Art. 1278.`, 10, 245);
+doc.text(`A empresa somente se responsabilizará pela entrega deste aparelho, mediante a apresentação deste comprovante`, 10, 160);
+doc.text(`após 90 dias a contar da data de entrega, a empresa disporá do equipamento afim de custear as despesas`, 10, 165);
+doc.text(`conforme, Art. 1275, 1592, 3995, do código civil brasileiro, sem prejuízo do Art. 1278.`,10,170);
+doc.text(``, 10, 245);
 
     
 
